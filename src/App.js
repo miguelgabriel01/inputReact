9180ca851760ba6b5e8bf8a54a21360a5858d937
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-04-27 23:31:45 -0300

    exemplo de envio de dados com o fatch de inputs no react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,81 @@
 import React from 'react';
+
+const formFields = [
+  {
+    id:'nome',
+    label:'nome',
+    type:'text'
+  },
+  {
+    id:'email',
+    label:'email',
+    type:'email'
+  },
+  {
+    id:'senha',
+    label:'senha',
+    type:'password'
+  },
+  {
+    id:'cep',
+    label:'cep',
+    type:'text'
+  },
+  {
+    id:'numero',
+    label:'numero',
+    type:'text'
+  },
+  {
+    id:'rua',
+    label:'rua',
+    type:'text'
+  },
+  {
+    id:'bairro',
+    label:'bairro',
+    type:'text'
+  },
+  {
+    id:'cidade',
+    label:'cidade',
+    type:'text'
+  },
+  {
+    id:'estado',
+    label:'estado',
+    type:'text'
+  },
+
+]
 const App = () => {
   const [form, setForm] = React.useState({
     nome: '',
     email: '',
+    senha:'',
+    cep:'',
+    numero:'',
+    rua:'',
+    bairro:'',
+    cidade:'',
+    estado:'',
   });
+
+  const [response,setResponse] = React.useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log(form);
+
+    fetch('https://ranekapi.origamid.dev/json/api/usuario',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',//nomes com "-" devem ser colocados entre ""
+      },
+      body:JSON.stringify(form)
+    }).then((response) => {
+      setResponse(response);
+    })
   }
 
   function handleChange({ target }) {
@@ -17,23 +85,21 @@ const App = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-
-      <label htmlFor="nome">Nome</label>
+     
+     {formFields.map(({id,label,type}) => (
+       <div key={id}>
+      <label htmlFor={id}>{label}</label><br/>
       <input 
-      type="text" 
-      id="nome" 
-      value={form.nome} 
+      type={type} 
+      id={id} 
+      value={form[id]} 
       onChange={handleChange} />
+         
+       </div>
+     ))}
 
-      {form.nome}
+     {response && response.ok && <p>Formulario enviado</p>}
 
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        value={form.email}
-        onChange={handleChange}
-      />
       <button>Enviar</button>
     </form>
   );
